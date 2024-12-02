22f831ad607646735f4dac36966e5af59b1b6c34
ManojkumarEdla9 <manojkumaredla9@gmail.com>
2024-12-02 14:39:12 +0530

    initial Commit of Amazon shopping-UI-clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Shop from './Pages/Shop/Shop';
+import Cart from './Pages//Cart/Cart';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { useData } from './Contexts/ProductContext';
+import Loading from './Components/Loading';
+import ProductDetails from './Components/ProductDetails/ProductDetails';
+import ErrorPage from './Pages/Error/ErrorPage';
+import SignIn from './Pages/User/UserSign/SignIn';
+import SignUp from './Pages/User/UserSign/SignUp';
+import Profile from './Pages/User/UserProfile/Profile';
 
-function App() {
+const App = () => {
+  const { loading } = useData();
+  const location = useLocation();
+  const shouldRenderHeaderAndFooter =
+    !location.pathname.includes('/sign-in') &&
+    !location.pathname.includes('/sign-up');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full bg-[#E3E6E6] min-h-screen min-w-[280px]">
+      {loading ? (
+        <Loading minH="min-h-screen" text="Amazon" />
+      ) : (
+        <>
+          {shouldRenderHeaderAndFooter && <Header />}
+          <main
+            className={`${
+              shouldRenderHeaderAndFooter && 'max-w-[1500px]'
+            } m-auto`}
+          >
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/shop/:categoryId" element={<Shop />} />
+              <Route
+                path="/shop/:categoryId/:id"
+                element={<ProductDetails />}
+              />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/sign-in" element={<SignIn />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </main>
+          {shouldRenderHeaderAndFooter && <Footer />}
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import { ProductProvider } from './Contexts/ProductContext';
+import { store } from './Redux/store';
+import { UserProvider } from './Contexts/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <UserProvider>
+        <ProductProvider>
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </ProductProvider>
+      </UserProvider>
+    </Provider>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
